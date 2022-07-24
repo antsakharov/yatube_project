1400cbe10fda2ca3fcdff6798097d9add57ffb15
Антон Сахаров <av.sakharov.888@gmail.com>
2022-07-25 00:14:10 +0300

    Include block extends

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,11 @@
 from importlib.resources import path
-from django.forms import SlugField
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):   
+    template = 'posts/index.html'
+    return render(request,template)
 def group_posts(request,slug):
-    return HttpResponse(f'Посты отфильтрованные по группам {slug}')
+    template = 'posts/group_list.html'
+    return render(request, template)
