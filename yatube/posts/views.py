a05c4b292adde46416f862993a27f042d93922e9
Антон Сахаров <av.sakharov.888@gmail.com>
2022-07-25 00:53:59 +0300

    Working links

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,10 @@
-from importlib.resources import path
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 # Главная страница
+#group/<slug:slug>
 def index(request):   
     template = 'posts/index.html'
     return render(request,template)
-def group_posts(request,slug):
+def group_posts(request):
     template = 'posts/group_list.html'
     return render(request, template)
